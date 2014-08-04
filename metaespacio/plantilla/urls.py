4ae5cf3543938f3f02cc37aa3b3998c642651bc9
Alberto Morales <amd77@galotecnia.com>
2014-08-04 19:21:42 +0100

    Aplicacion para plantilla base bootstrap

---
+++ metaespacio/plantilla/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
+)
