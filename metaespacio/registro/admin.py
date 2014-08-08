d4bb056d18e0c2757af509c48020e1b3bbbd0e45
torehc <torehc@gmail.com>
2014-08-08 10:15:32 +0100

    Añadido Filtro en Panel Admin - Registro

--- metaespacio/registro/admin.py
+++ metaespacio/registro/admin.py
@@ -5,11 +5,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 
+
 class UsuarioAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         (_('Extra fields'), {'fields': ('dni', 'direccion', 'telefono')}),
     )
     pass
 
+
+class Datos(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'dni', 'telefono')
+#    list_filter = ['']
+
+
 admin.site.unregister(User)
-admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Usuario, Datos)
