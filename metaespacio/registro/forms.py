4303ba893b1bc09df59209ba01dd6cc7c946e146
Alberto Morales <amd77@galotecnia.com>
2014-08-14 18:14:12 +0100

    Now it registers gracefully but redirect url is wrong

--- metaespacio/registro/forms.py
+++ metaespacio/registro/forms.py
@@ -3,11 +3,61 @@
 from django import forms
 from captcha.fields import CaptchaField
 from .models import Usuario
+from django.contrib.auth.models import User
+import string
+
+
+def cuenta_upper_lower_digits_other(cadena):
+    upper = lower = digits = other = 0
+    for ch in cadena:
+        if ch in string.lowercase:
+            lower += 1
+        elif ch in string.uppercase:
+            upper += 1
+        elif ch in string.digits:
+            digits += 1
+        else:
+            other += 1
+    return upper, lower, digits, other
+
 
 class UsuarioForm(forms.ModelForm):
+    password1 = forms.CharField(label=u"Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label=u"Contraseña", widget=forms.PasswordInput)
     captcha = CaptchaField()
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username__iexact=username).count() > 0:
+            raise forms.ValidationError(u"El nombre de usuario está en uso")
+        return username
+
+    def clean_password1(self):
+        passwd = self.cleaned_data['password1']
+        upper, lower, digits, other = cuenta_upper_lower_digits_other(passwd)
+        required = []
+        if len(passwd) < 8:
+            required.append(u"8 caracteres")
+        if upper == 0:
+            required.append(u"una mayúscula")
+        if lower == 0:
+            required.append(u"una minúscula")
+        if digits == 0:
+            required.append(u"un número")
+        if len(required) > 0:
+            msg = u"Requerido: {}".format(", ".join(required))
+            raise forms.ValidationError(msg)
+        return passwd
+
+    def clean(self):
+        cleaned_data = super(UsuarioForm, self).clean()
+        p1 = cleaned_data.get('password1')
+        p2 = cleaned_data.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError("Las contraseñas deben coincidir")
+        return cleaned_data
+
     class Meta:
         model = Usuario
         fields = ['first_name', 'last_name', 'username', 'email',
                   'dni', 'direccion', 'telefono']
-
--- metaespacio/registro/views.py
+++ metaespacio/registro/views.py
@@ -11,6 +11,7 @@ class CreateUser(CreateView):
     def form_valid(self, form):
         redirect_url = super(CreateUser, self).form_valid(form)
         self.object.is_active = False
+        self.object.set_password(form.cleaned_data["password1"])
         self.object.save()
         return redirect_url
 
