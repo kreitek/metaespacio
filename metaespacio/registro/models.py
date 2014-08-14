55ac76c3538537e1f58da0d48d0adb6ab9279804
Alberto Morales <amd77@galotecnia.com>
2014-08-14 18:19:46 +0100

    Needed for Generic Editing views

--- metaespacio/registro/models.py
+++ metaespacio/registro/models.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.db import models
 
-from django.contrib.auth.models import User
 
 class Usuario(User):
     dni = models.CharField(max_length=9)
     direccion = models.TextField(help_text=u"Dirección postal completa, puede tener varias líneas")
     telefono = models.CharField(max_length=9)
+
+    def get_absolute_url(self):
+        return reverse("detail_user", kwargs={'pk': self.pk})
