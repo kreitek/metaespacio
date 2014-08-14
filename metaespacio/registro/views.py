944634d0582c0cf4d055aeb9ac73f7162ac15da4
Alberto Morales <amd77@galotecnia.com>
2014-08-14 18:48:43 +0100

    Added login required, and added search of users using username

--- metaespacio/registro/views.py
+++ metaespacio/registro/views.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from django.views.generic import CreateView, DetailView
+from django.contrib.auth.decorators import login_required
 
 from .models import Usuario
 from .forms import UsuarioForm
+
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
 
 
 class CreateUser(CreateView):
@@ -10,12 +20,13 @@ class CreateUser(CreateView):
 
     def form_valid(self, form):
         redirect_url = super(CreateUser, self).form_valid(form)
-        self.object.is_active = False
+        # FIXME usar Django-registration http://django-registration.readthedocs.org/
+        # self.object.is_active = False
         self.object.set_password(form.cleaned_data["password1"])
         self.object.save()
         return redirect_url
 
 
-class DetailUser(DetailView):
+class DetailUser(LoginRequiredMixin, DetailView):
     model = Usuario
     template_name = "registro/usuario_detalle.html"
