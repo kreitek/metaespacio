d44768f52acf3736281b51a1d62b44c652a35c52
Alberto Morales <amd77@galotecnia.com>
2014-08-14 17:57:22 +0100

    Not needed

--- metaespacio/registro/urls.py
+++ metaespacio/registro/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from .views import CreateUser, DetailUser, LoginUser
+from .views import CreateUser, DetailUser
 
 urlpatterns = patterns('',
     url(r'^create(.html)?$', CreateUser.as_view(), name="create_user"),
     url(r'^show/(?P<pk>\d+)/$', DetailUser.as_view(), name="detail_user"),
-    url(r'^login(.html)?$', LoginUser.as_view(), name="login_user"),
     url(r'^$', CreateUser.as_view(), name="create_user"),
 )
