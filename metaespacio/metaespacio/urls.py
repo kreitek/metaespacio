7609b61dd127b4e6e3c7eb8eda94d2475510200a
torehc <torehc@gmail.com>
2014-08-19 11:11:56 +0100

    Add simple page of information

--- metaespacio/metaespacio/urls.py
+++ metaespacio/metaespacio/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^registro/', include('registro.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^captcha/', include('captcha.urls')),
+    url(r'^informacion/$', TemplateView.as_view(template_name='informacion.html')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="login"),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name="logout"),
     url(r'^', include('plantilla.urls')),
