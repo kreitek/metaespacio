011a046c67df02c165a08cc25d0989babecea19c
Alberto Morales <amd77@galotecnia.com>
2015-05-03 19:44:57 +0100

    bug cuando no hay site_id en settings.py ni site coincidente

--- metaespacio/common/context_processor.py
+++ metaespacio/common/context_processor.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
+from django.contrib.sites.models import Site
 from django.contrib.sites.shortcuts import get_current_site
 from espacios.models import Espacio
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 def site(request):
-    site = get_current_site(request)
-    espacio = site.espacio_set.first()
-    espacios = Espacio.objects.all()
-    return {'site': site, 'espacio': espacio, 'espacios': espacios}
+    kwargs = {
+        'espacios': Espacio.objects.all(),
+    }
+    try:
+        kwargs['site'] = get_current_site(request)
+        kwargs['espacio'] = kwargs['site'].espacio_set.first()
+    except Site.DoesNotExist:
+        kwargs['espacio'] = kwargs['site'] = None
+        logger.error("Site does not exist: {}".format(request.get_host()))
+    return kwargs
