97e0a66f1dff816619d925578e63ffa431f8b607
Alberto Morales <amd77@galotecnia.com>
2015-05-10 15:19:19 +0100

    Resolviendo bug #8 que es porque el username tenia un '-'

--- metaespacio/registro/urls.py
+++ metaespacio/registro/urls.py
@@ -4,6 +4,6 @@ from . import views as v
 urlpatterns = patterns(
     '',
     url(r'^create$', v.CreateUser.as_view(), name="create_user"),
-    url(r'^(?P<slug>\w+)$', v.DetailUser.as_view(slug_field="username"), name="detail_user"),
+    url(r'^(?P<slug>[\w_-]+)$', v.DetailUser.as_view(slug_field="username"), name="detail_user"),
     # url(r'^show/(?P<pk>\d+)/$', DetailUser.as_view(), name="detail_user"),
 )
