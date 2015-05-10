11039e6e157adea99173aa094b594de0e307db4b
Alberto Morales <amd77@galotecnia.com>
2015-05-10 20:30:17 +0100

    Comienzo de resolucion del bug #4: ya hay una vista /graph que muestra una tabla html con el mes y la cantidad... falta elegir el framework y javascriptarlo

--- metaespacio/cuotas/urls.py
+++ metaespacio/cuotas/urls.py
@@ -3,5 +3,6 @@ from . import views as v
 
 urlpatterns = patterns(
     '',
-    url(r'^mensualidades/(?P<username>\w+)$', v.MensualidadList.as_view(), name="mensualidad_list"),
+    url(r'^mensualidades/(?P<username>[\w_-]+)$', v.MensualidadList.as_view(paginate_by=10), name="mensualidad_list"),
+    url(r'^mensualidades/(?P<username>[\w_-]+)/graph$', v.MensualidadListGraph.as_view(), name="mensualidad_list_graph"),
 )
--- metaespacio/cuotas/views.py
+++ metaespacio/cuotas/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 from .models import Mensualidad
 from espacios.views import SiteMixin
+from collections import OrderedDict
 
 
 class MensualidadList(SiteMixin, ListView):
@@ -18,3 +19,20 @@ class MensualidadList(SiteMixin, ListView):
         return super(MensualidadList, self).get_queryset().filter(
             miembro__user=url_user,
             miembro__espacio__site=self.site)
+
+
+class MensualidadListGraph(MensualidadList):
+    def get_template_names(self):
+        return ["cuotas/mensualidad_list_graph.html"]
+
+    def get_context_data(self, **kwargs):
+        context = super(MensualidadListGraph, self).get_context_data(**kwargs)
+        sumas_mensuales = OrderedDict()
+        for mensualidad in self.get_queryset():
+            mes = mensualidad.fecha.replace(day=1)
+            if mes not in sumas_mensuales:
+                sumas_mensuales[mes] = mensualidad.cantidad
+            else:
+                sumas_mensuales[mes] += mensualidad.cantidad
+        context['sumas'] = sumas_mensuales
+        return context
