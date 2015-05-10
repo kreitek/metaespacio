4d18bc45b68b6d3e411dbc0cad2a108f27c8c815
Alberto Morales <amd77@galotecnia.com>
2015-05-10 21:39:52 +0100

    definicion demasiado abierta

--- metaespacio/pages/urls.py
+++ metaespacio/pages/urls.py
@@ -5,6 +5,6 @@ from . import views as v
 
 urlpatterns = patterns(
     '',
-    url(r'^(?P<slug>.+)$', v.PaginaView.as_view(), name="pagina_view"),
+    url(r'^(?P<slug>[\w_-]+)$', v.PaginaView.as_view(), name="pagina_view"),
     url(r'^$', v.PaginaIndex.as_view(), name="inicio"),
 )
