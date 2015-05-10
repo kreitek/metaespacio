7351e7e7e102b8556e7d782fb0c08fa414e83ddf
J Lucas Grillo <jlucasgl@gmail.com>
2015-05-10 18:34:32 +0100

    Issue #6: Usuarios No Socio sin mensajes. Completando el perfil de usuario para reflejar membresia distinguiendo socio de no socio

--- metaespacio/registro/views.py
+++ metaespacio/registro/views.py
@@ -2,8 +2,10 @@
 
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.views.generic import CreateView, DetailView
 from common.views import LoginRequiredMixin
+from espacios.models import Miembro
 from .forms import UserForm
 
 
@@ -25,3 +27,14 @@ class DetailUser(LoginRequiredMixin, DetailView):
     model = User
     template_name = "registro/usuario_detalle.html"
     context_object_name = "this_user"  # importante para no sobreescribir {{user}} de auth
+
+    def dispatch(self, request, *args, **kwargs):
+        self.url_user = get_object_or_404(User, username=self.kwargs['slug'])
+        self.espacios = get_list_or_404(Miembro, user=self.request.user.pk)
+        return super(DetailUser, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailUser, self).get_context_data(**kwargs)
+        context[self.context_object_name] = self.url_user
+        context['espacios'] = self.espacios
+        return context
