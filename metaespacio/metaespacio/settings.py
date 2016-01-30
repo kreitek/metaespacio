af6d3eb93114a584377925838d1ad6c6f109394c
root <root@kreitek.org>
2016-01-30 13:08:41 +0000

    produccion vs debug y envio de correos salientes

--- metaespacio/metaespacio/settings.py
+++ metaespacio/metaespacio/settings.py
@@ -20,6 +20,7 @@ SECRET_KEY = '57#f7u+v@yh*vwv^ox#%*wgx6c@_a*%8#)0@1f6#dt=oar4u$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getuid() >= 1000)
+PRODUCCION = False
 TEMPLATE_DEBUG = DEBUG
 
 
@@ -100,6 +101,16 @@ TEMPLATE_LOADERS = ('django.template.loaders.app_directories.Loader', )
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+# cosas custom
+MEDIA_URL = "/media/"
+STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = "/"
+CRISPY_TEMPLATE_PACK = "bootstrap3"
+SITE_URL = 'http://metaespacio.org'
+LOGIN_URL = '/accounts/login'
+OIDC_RSA_KEY_FOLDER = BASE_DIR
+DEFAULT_FROM_EMAIL = 'cambiame@que.soy.util'
+
 try:
     from .settings_local import *  # noqa
 except ImportError:
@@ -109,22 +120,17 @@ if DEBUG:
     # static en desarrollo en carpeta del proyecto
     STATIC_ROOT = os.path.join(BASE_DIR, '.static')
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-    # errores por consola
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     # en desarrollo no se usa
     ALLOWED_HOSTS = []
     INSTALLED_APPS += ('debug_toolbar', )
-else:
+
+if PRODUCCION:
     # FIXME Esto revisarlo porque tampoco lo estamos poniendo aqui exactamente
     STATIC_ROOT = '/var/www/metaespacio/static/'
     MEDIA_ROOT = '/opt/metaespacio/media/'
     # en preproduccion o produccion si se usa
     ALLOWED_HOSTS = ['*']
+else:
+    # errores por consola
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-MEDIA_URL = "/media/"
-STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = "/"
-CRISPY_TEMPLATE_PACK = "bootstrap3"
-SITE_URL = 'http://metaespacio.org'
-LOGIN_URL = '/accounts/login'
-OIDC_RSA_KEY_FOLDER = BASE_DIR
