2dbe062cbab9c30ef2d132d9fed4b5959b230ba3
J. Lucas Grillo <jlucas.gl@gmail.com>
2016-02-28 13:43:54 +0000

    Añadido is_active a lista admin de usuarios

--- metaespacio/espacios/admin.py
+++ metaespacio/espacios/admin.py
@@ -23,7 +23,7 @@ class MiembroAdmin(admin.ModelAdmin):
 
 
 class UserAdmin3(UserAdmin2):
-    list_display = ['username', 'date_joined', 'email', 'first_name', 'last_name', 'is_staff']
+    list_display = ['username', 'date_joined', 'email', 'first_name', 'last_name', 'is_active', 'is_staff']
     ordering = ['date_joined', 'username']
     inlines = UserAdmin2.inlines + [MiembroInline]
 
