f1901abad00d4a002529f5b7a1a9f260492e226a
Alberto Morales <amd77@galotecnia.com>
2016-11-27 18:15:39 +0000

    Poder clickar en las cuentas para que filtre

--- metaespacio/contabilidad/admin.py
+++ metaespacio/contabilidad/admin.py
@@ -1,6 +1,8 @@
+from __future__ import unicode_literals
 from django.contrib import admin
-from .models import Cuenta, Asiento, Linea
+from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
+from .models import Cuenta, Asiento, Linea
 
 
 class LineaInline(admin.TabularInline):
@@ -15,7 +17,10 @@ class AsientoAdmin(admin.ModelAdmin):
     inlines = [LineaInline]
 
     def lineas(self, obj):
-        return mark_safe("<br/>".join([unicode(x) for x in obj.linea_set.all()]))
+        url = reverse("admin:contabilidad_asiento_changelist")
+        show = lambda x: '{:.2f} <a href="{}?linea__cuenta__id__exact={}">{}</a>'.format(
+            x.cantidad, url, x.cuenta.id, x.cuenta.nombre)
+        return mark_safe("<br/>".join([show(x) for x in obj.linea_set.all()]))
 
     def correcto(self, obj):
         d = obj.diferencia()
