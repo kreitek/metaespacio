a43ffe4f54c8d309ecc5178560c6d17b7f10fab1
Alberto Morales <amd77@galotecnia.com>
2016-12-18 20:46:03 +0000

    Mejorando la presentacion del asiento

--- metaespacio/contabilidad/templatetags/asiento.py
+++ metaespacio/contabilidad/templatetags/asiento.py
@@ -10,14 +10,24 @@ def asiento_resumen(asiento, prefijo=None):
     qs = asiento.linea_set.all()
     if prefijo:
         qs = qs.filter(cuenta__nombre__startswith=prefijo)
-    cantidad = 0.0
-    sumal = []
-    sumar = []
+    conceptos1, cantidades1 = [], []
+    conceptos2, cantidades2 = [], []
     for l in qs:
-        valor = l.cantidad
-        if valor > 0:
-            cantidad += l.cantidad
-            sumar.append(l.cuenta.ultimo_nombre(prefijo))
-        else:
-            sumal.append(l.cuenta.ultimo_nombre(prefijo))
-    return "{}->{} ({:.2f})".format(",".join(sumal), ",".join(sumar), cantidad)
+        if l.cantidad < 0:
+            conceptos1.append(l.cuenta.ultimo_nombre(prefijo))
+            cantidades1.append(l.cantidad)
+        elif l.cantidad > 0:
+            conceptos2.append(l.cuenta.ultimo_nombre(prefijo))
+            cantidades2.append(l.cantidad)
+    if len(conceptos1) == len(conceptos2) == 1:
+        return "{}->{}({:.2f})".format(
+            ",".join(conceptos1),
+            ",".join(conceptos2),
+            cantidades2[0])
+    else:
+        return "{}({}) -> {}({})".format(
+            ",".join(conceptos1),
+            "+".join(["{:.2f}".format(-x) for x in cantidades1]),
+            ",".join(conceptos2),
+            "+".join(["{:.2f}".format(x) for x in cantidades2]),
+            )
