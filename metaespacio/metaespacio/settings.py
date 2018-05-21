08cea89447a1a8cbff82f7528e26db07e4e217b0
root <root@kreitek.org>
2018-05-21 18:37:20 +0100

    Cambios por desactualizacion de versiones

--- metaespacio/metaespacio/settings.py
+++ metaespacio/metaespacio/settings.py
@@ -164,5 +164,6 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
