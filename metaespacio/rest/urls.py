4879e1175dd1573cbf06ee94b419884476b8fdd0
Dani Alvarez <danidask@gmail.com>
2019-06-23 19:50:41 +0100

    interface para spaceapi.io

--- metaespacio/rest/urls.py
+++ metaespacio/rest/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-import views
+from . import views as v
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'miembros', views.MiembroViewSet)
+router.register(r'users', v.UserViewSet)
+router.register(r'groups', v.GroupViewSet)
+router.register(r'miembros', v.MiembroViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^spaceapi/', v.spaceapi),
+
 ]
--- metaespacio/rest/views.py
+++ metaespacio/rest/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User, Group
+from django.http import JsonResponse
+
 from espacios.models import Miembro
 from rest_framework import viewsets
-from serializers import MiembroSerializer, UserSerializer, GroupSerializer
+from .serializers import MiembroSerializer, UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -27,3 +29,36 @@ class MiembroViewSet(viewsets.ModelViewSet):
     queryset = Miembro.objects.all().order_by('-fecha_alta')
     serializer_class = MiembroSerializer
 
+def spaceapi(request):
+    # https://spaceapi.io/
+    # TODO esta hardcodeado para probar, pasar a DB o a settings
+    context = {
+      "api": "0.13",
+      "space": "Kreitek",
+      "logo": "https://kreitek.org//sites/default/files/kreitek_imagen.jpg",
+      "url": "https://kreitek.org",
+      "location": {
+        "address": "Colegio Nuryana, Calle San Francisco de Paula, San Cristobal de La Laguna, Santa Cruz de Tenerife, Spain",
+        "lon": 28.4787801,
+        "lat": -16.3256688
+      },
+      "contact": {
+        "email": "info@kreitek.org",
+        "twitter": "@kreitek"
+      },
+      "issue_report_channels": [
+        "email"
+      ],
+      "state": {
+        "icon": {
+          "open": "https://kreitek.org//sites/default/files/site_open.gif",
+          "closed": "https://kreitek.org//sites/default/files/site_closed.gif"
+        },
+        "open": False
+      },
+      "projects": [
+        "https://github.com/kreitek",
+        "https://wiki.kreitek.org"
+      ]
+    }
+    return JsonResponse(context)
