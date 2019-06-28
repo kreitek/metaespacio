4ae8861fd686e743d26aacb2c1ca7e1013f0077b
Dani Alvarez <danidask@gmail.com>
2019-06-28 12:54:09 +0100

    habilitar CORS solo para endpoint spaceapi

--- metaespacio/rest/views.py
+++ metaespacio/rest/views.py
@@ -29,36 +29,42 @@ class MiembroViewSet(viewsets.ModelViewSet):
     queryset = Miembro.objects.all().order_by('-fecha_alta')
     serializer_class = MiembroSerializer
 
+
 def spaceapi(request):
     # https://spaceapi.io/
     # TODO esta hardcodeado para probar, pasar a DB o a settings
     context = {
-      "api": "0.13",
-      "space": "Kreitek",
-      "logo": "https://kreitek.org//sites/default/files/kreitek_imagen.jpg",
-      "url": "https://kreitek.org",
-      "location": {
-        "address": "Colegio Nuryana, Calle San Francisco de Paula, San Cristobal de La Laguna, Santa Cruz de Tenerife, Spain",
-        "lon": 28.4787801,
-        "lat": -16.3256688
-      },
-      "contact": {
-        "email": "info@kreitek.org",
-        "twitter": "@kreitek"
-      },
-      "issue_report_channels": [
-        "email"
-      ],
-      "state": {
-        "icon": {
-          "open": "https://kreitek.org//sites/default/files/site_open.gif",
-          "closed": "https://kreitek.org//sites/default/files/site_closed.gif"
+        "api": "0.13",
+        "space": "Kreitek",
+        "logo": "https://kreitek.org//sites/default/files/kreitek_imagen.jpg",
+        "url": "https://kreitek.org",
+        "location": {
+            "address": "Colegio Nuryana, Calle San Francisco de Paula, San Cristobal de La Laguna, Santa Cruz de Tenerife, Spain",
+            "lon": 28.4787801,
+            "lat": -16.3256688
         },
-        "open": False
-      },
-      "projects": [
-        "https://github.com/kreitek",
-        "https://wiki.kreitek.org"
-      ]
+        "contact": {
+            "email": "info@kreitek.org",
+            "twitter": "@kreitek"
+        },
+        "issue_report_channels": [
+            "email"
+        ],
+        "state": {
+            "icon": {
+                "open": "https://kreitek.org//sites/default/files/site_open.gif",
+                "closed": "https://kreitek.org//sites/default/files/site_closed.gif"
+            },
+            "open": False
+        },
+        "projects": [
+            "https://github.com/kreitek",
+            "https://wiki.kreitek.org"
+        ]
     }
-    return JsonResponse(context)
+    response = JsonResponse(context)
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "900"
+    response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+    return response
