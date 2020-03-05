6d34184b656b60c22643d218d15600c45f6d0008
Alberto Morales <amd77@galotecnia.com>
2020-03-05 17:06:14 +0000

    Instalador/ejecutador

---
+++ metaespacio/manage.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+log () {
+    echo ----------------------------------------------------------------------
+    echo $*
+}
+
+set -e
+
+test -e .virtualenv || {
+    log creando .virtualenv
+    virtualenv .virtualenv --python=python2
+}
+
+log Activando .virtualenv
+. .virtualenv/bin/activate
+
+log Instalando .virtualenv
+pip install -r requirements.txt 
+
+# log "Safety check (un mes)"
+# pip install safety
+# pip freeze | safety check --bare
+
+log Aplicando migraciones
+./manage.py migrate
+
+SUPERUSERS=$(./manage.py shell -c 'from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).count())')
+test $SUPERUSERS = 0 && {
+    log Creando superusuario
+    ./manage.py createsuperuser --username admin --email root@galotecnia.com --noinput
+}
+
+log ./manage.py check
+./manage.py check
+
+log ./manage.py $*
+./manage.py $*
+
